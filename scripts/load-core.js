75de251d8d6dd6c3a23f184344571af20395180a
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-03-30 23:10:49 +0200

    corrected order of head elements & cleaned void

--- scripts/load-core.js
+++ scripts/load-core.js
@@ -9,28 +9,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 async function loadCoreContent(templates) {
-	//const nav = templates.getElementById("nav-tmpl");
-	//const header = document.createElement("header");
-	//header.appendChild(nav.content);
-	//
-	//// Insert as the first child
-	//document.body.insertBefore(header, document.body.firstChild);
-
-	// Add head items
-	templates
-		.getElementsByTagName("head")[0]
-		.childNodes.forEach((tag) =>
+	Array.from(templates.getElementsByTagName("head")[0].childNodes)
+		.slice()
+		.reverse()
+		.forEach((tag) =>
 			document.head.insertBefore(tag, document.head.firstChild)
 		);
 
-	// Add body items
-	templates.getElementsByTagName("body")[0].childNodes.forEach((tag) => {
+	templates.getElementsByTagName("scripts")[0].childNodes.forEach((tag) => {
 		if (tag.nodeType === Node.ELEMENT_NODE) {
 			document.body.appendChild(tag);
 		}
 	});
 
-	// Remove voided items
 	Array.from(document.getElementsByClassName("short-lived")).forEach((tag) =>
 		tag.remove()
 	);
