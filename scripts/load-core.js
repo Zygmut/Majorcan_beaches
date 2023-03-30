1e04d4441f591f17462b34188869c80e0cee515d
Zygmut <deltas.game@gmail.com>
2023-03-30 15:55:36 +0200

    first iteration

---
+++ scripts/load-core.js
@@ -0,0 +1,33 @@
+window.onload = (async () => {
+	const templates = await fetch("../void.html")
+		.then((response) => response.text())
+		.then((html) => {
+			return new DOMParser().parseFromString(html, "text/html");
+		})
+		.catch((error) => console.error(error));
+
+	//const nav = templates.getElementById("nav-tmpl");
+	//const header = document.createElement("header");
+	//header.appendChild(nav.content);
+	//
+	//// Insert as the first child
+	//document.body.insertBefore(header, document.body.firstChild);
+
+	// Add head items
+	templates
+		.getElementsByTagName("head")[0]
+		.childNodes.forEach((tag) =>
+			document.head.insertBefore(tag, document.head.firstChild)
+		);
+
+	// Add body items
+	templates.getElementsByTagName("body")[0].childNodes.forEach((tag) => {
+		console.log(tag);
+		document.body.appendChild(tag);
+	});
+
+	// Remove voided items
+	Array.from(document.getElementsByClassName("short-lived")).forEach((tag) =>
+		tag.remove()
+	);
+})();
