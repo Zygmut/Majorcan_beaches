3753ebf8267f9dbfdb79996bd3067ea1fb134abc
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-04-02 17:51:50 +0200

    format

--- scripts/load-core.js
+++ scripts/load-core.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 	addMobileNavSupport();
 	addPreloader();
 	removeShortLivedElements();
